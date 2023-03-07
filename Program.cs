74dbb9bc9a4cbbb1db5b34e2c00e79158b52d7fa
Rodrigo <rodrigogrigoleto@gmail.com>
2023-03-07 19:35:27 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Primeira alteração feita no master
+            Console.WriteLine("Primeira Alteração - master");
         }
     }
 }
