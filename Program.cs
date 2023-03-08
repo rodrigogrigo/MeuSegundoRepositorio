6aa4d48d54d112b59ad81b3412a8510e03f0dce2
Rodrigo <rodrigogrigoleto@gmail.com>
2023-03-07 21:22:51 -0300

    Alteração do main

--- Program.cs
+++ Program.cs
@@ -7,18 +7,7 @@
             // Primeira alteração feita no master
             Console.WriteLine("Primeira Alteração - master");
 
-            // Segunda alteração da master
-            Console.WriteLine("Segunda Alteração - master");
-
-            // Segunda implementação do fonte
-            // 1ª Alteração do branch correção
-            Console.WriteLine("Alteração do branch correção.");
-
-            // Terceira alteração da master
-            Console.WriteLine("Terceira Alteração - master");
-
-            // Quarta alteração da master
-            Console.WriteLine("Quarta Alteração - master");
+            // Apaguei tudo
         }
     }
 }
